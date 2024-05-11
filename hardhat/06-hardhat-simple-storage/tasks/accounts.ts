f11c13a6c6115e5f6277f164c683f8b57a3a0a90
Rodrigo Sarmiento <info@rodrigosarmiento.com>
2024-05-11 15:58:29 -0600

    refactor: re-create the accounts task

--- hardhat/06-hardhat-simple-storage/tasks/accounts.ts
+++ hardhat/06-hardhat-simple-storage/tasks/accounts.ts
@@ -1,10 +1,8 @@
-// import { ethers } from 'hardhat';
-// import { task } from 'hardhat/config';
+import { task } from 'hardhat/config';
 
-// task('accounts', 'Prints the list of accounts', async () => {
-//     const accounts = await ethers.getSigners();
-
-//     for (const account of accounts) {
-//         console.log(account.address);
-//     }
-// });
+task('accounts', 'Prints the list of accounts', async (_, { ethers }) => {
+    const accounts = await ethers.getSigners();
+    for (const account of accounts) {
+        console.log(account.address);
+    }
+});
