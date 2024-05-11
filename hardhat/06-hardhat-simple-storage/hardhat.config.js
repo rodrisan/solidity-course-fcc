6943862f7dd5f904450af51be9cd27b5bcdbdff3
Rodrigo Sarmiento <info@rodrigosarmiento.com>
2024-05-11 11:24:43 -0600

    feat: basic deploy using hardhat
    Also to Sepolia:
    https://sepolia.etherscan.io/address/0x60f2D027B5c687Ed9EB4BCA4286e24D588231a90

--- hardhat/06-hardhat-simple-storage/hardhat.config.js
+++ hardhat/06-hardhat-simple-storage/hardhat.config.js
@@ -8,10 +8,22 @@ require('@nomicfoundation/hardhat-verify');
  * @type import('hardhat/config').HardhatUserConfig
  */
 
+const SEPOLIA_RPC_URL =
+    process.env.SEPOLIA_RPC_URL ||
+    'https://eth-sepolia.g.alchemy.com/v2/your-api-key';
+const PRIVATE_KEY =
+    process.env.PRIVATE_KEY ||
+    '0x11ee3108a03081fe260ecdc106554d09d9d1209bcafd46942b10e02943effc4a';
+
 module.exports = {
     defaultNetwork: 'hardhat',
     networks: {
         hardhat: {},
+        sepolia: {
+            url: SEPOLIA_RPC_URL,
+            accounts: [PRIVATE_KEY],
+            chainId: 11155111,
+        },
         localhost: {
             url: 'http://localhost:8545',
             chainId: 31337,
