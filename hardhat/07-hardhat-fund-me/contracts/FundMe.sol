c7edeb316d6768a6ec7cbc2e0bbf5d664ef5828d
Rodrigo Sarmiento <info@rodrigosarmiento.com>
2024-05-11 20:58:47 -0600

    feat(lesson07): add project base

---
+++ hardhat/07-hardhat-fund-me/contracts/FundMe.sol
@@ -0,0 +1,61 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import './PriceConverter.sol';
+
+error NotOwner();
+
+contract FundMe {
+    using PriceConverter for uint256;
+
+    uint256 public constant MINIMUM_USD = 50 * 10 ** 18;
+    address[] public funders;
+    address public immutable i_owner;
+
+    mapping(address => uint256) public addressToAmountFunded;
+
+    constructor() {
+        i_owner = msg.sender;
+    }
+
+    function fund() public payable {
+        require(
+            msg.value.getConversionRate() >= MINIMUM_USD,
+            'You need to spend more ETH!'
+        ); // 1e18 =1 * 10 ** 18
+        funders.push(msg.sender);
+        addressToAmountFunded[msg.sender] = msg.value;
+    }
+
+    function withdraw() public onlyOwner {
+        for (
+            uint256 funderIndex = 0;
+            funderIndex < funders.length;
+            funderIndex++
+        ) {
+            address funder = funders[funderIndex];
+            addressToAmountFunded[funder] = 0;
+        }
+        funders = new address[](0);
+
+        (bool callSuccess, ) = payable(msg.sender).call{
+            value: address(this).balance
+        }('');
+
+        require(callSuccess, 'Call failed');
+    }
+
+    modifier onlyOwner() {
+        if (msg.sender != i_owner) revert NotOwner();
+        _;
+    }
+
+    fallback() external payable {
+        fund();
+    }
+
+    receive() external payable {
+        fund();
+    }
+}
