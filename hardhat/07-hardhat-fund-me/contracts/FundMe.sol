55af9b7290f9ac77e173859162500e8a42e3a587
Rodrigo Sarmiento <info@rodrigosarmiento.com>
2024-05-13 08:51:11 -0600

    feat(lesson07): accomplish with Solidity Style Guide

--- hardhat/07-hardhat-fund-me/contracts/FundMe.sol
+++ hardhat/07-hardhat-fund-me/contracts/FundMe.sol
@@ -1,32 +1,68 @@
 // SPDX-License-Identifier: MIT
-
+// 1. Pragma
 pragma solidity ^0.8.0;
 
+// 2. Imports
+import '@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol';
 import './PriceConverter.sol';
 
-error NotOwner();
+// 3. Interfaces, Libraries, Contracts
+error FundMe__NotOwner();
 
+/**
+ * @title A contract for crowd funding
+ * @author Rod
+ * @notice This contract is to demo a sample fundind contract
+ * @dev This implements price feeds as our library
+ */
 contract FundMe {
+    // Type declarations
     using PriceConverter for uint256;
 
+    // State variables
     uint256 public constant MINIMUM_USD = 50 * 10 ** 18;
-    address[] public funders;
     address public immutable i_owner;
-
+    address[] public funders;
     mapping(address => uint256) public addressToAmountFunded;
-
     AggregatorV3Interface public priceFeed;
+
+    // Events (none by this time)
+
+    // Modifiers
+    modifier onlyOwner() {
+        if (msg.sender != i_owner) revert FundMe__NotOwner();
+        _;
+    }
+
+    // Functions Order:
+    //// constructor
+    //// receive
+    //// fallback
+    //// external
+    //// public
+    //// internal
+    //// private
+    //// view / pure
 
     constructor(address priceFeedAddress) {
         i_owner = msg.sender;
         priceFeed = AggregatorV3Interface(priceFeedAddress);
     }
 
+    receive() external payable {
+        fund();
+    }
+
+    fallback() external payable {
+        fund();
+    }
+
+    /// @notice Funds our contract based on the ETH/USD price
     function fund() public payable {
         require(
             msg.value.getConversionRate(priceFeed) >= MINIMUM_USD,
             'You need to spend more ETH!'
-        ); // 1e18 =1 * 10 ** 18
+        );
         funders.push(msg.sender);
         addressToAmountFunded[msg.sender] = msg.value;
     }
@@ -47,18 +83,5 @@ contract FundMe {
         }('');
 
         require(callSuccess, 'Call failed');
-    }
-
-    modifier onlyOwner() {
-        if (msg.sender != i_owner) revert NotOwner();
-        _;
-    }
-
-    fallback() external payable {
-        fund();
-    }
-
-    receive() external payable {
-        fund();
     }
 }
