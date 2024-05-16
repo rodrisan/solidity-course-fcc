7ddd4afeea74a268a67930aabee5e19c7dc031cc
Rodrigo Sarmiento <info@rodrigosarmiento.com>
2024-05-16 15:18:37 -0600

    test(lesson07): add fund Script

---
+++ hardhat/07-hardhat-fund-me/scripts/fund.js
@@ -0,0 +1,27 @@
+/*
+ * This script funds the FundMe contract with 0.1 ETH.
+ * Run it with
+ *  `yarn hardhat node
+ *   yarn hardhat run scripts/fund.js --network localhost
+ *  `
+ */
+const { ethers, getNamedAccounts } = require('hardhat');
+
+async function main() {
+    const { deployer } = await getNamedAccounts();
+    const fundMe = await ethers.getContract('FundMe', deployer);
+    console.log(`Got contract FundMe at ${fundMe.address}`);
+    console.log('Funding contract...');
+    const transactionResponse = await fundMe.fund({
+        value: ethers.utils.parseEther('0.1'),
+    });
+    await transactionResponse.wait();
+    console.log('Funded!');
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
