d5e3e8de5c833157f8e4973efc944e9d36262176
Rodrigo Sarmiento <info@rodrigosarmiento.com>
2024-06-06 08:56:15 -0600

    feat(lesson13): get WETH

---
+++ hardhat/13-hardhat-defi/hardhat.config.js
@@ -0,0 +1,113 @@
+require('@nomiclabs/hardhat-waffle');
+require('@nomiclabs/hardhat-etherscan');
+require('hardhat-deploy');
+require('solidity-coverage');
+require('hardhat-gas-reporter');
+require('dotenv').config();
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+
+const MAINNET_RPC_URL =
+    process.env.MAINNET_RPC_URL ||
+    process.env.ALCHEMY_MAINNET_RPC_URL ||
+    'https://eth-mainnet.alchemyapi.io/v2/your-api-key';
+const SEPOLIA_RPC_URL =
+    process.env.SEPOLIA_RPC_URL || 'https://eth-sepolia.g.alchemy.com/v2/YOUR-API-KEY';
+const POLYGON_MAINNET_RPC_URL =
+    process.env.POLYGON_MAINNET_RPC_URL || 'https://polygon-mainnet.alchemyapi.io/v2/your-api-key';
+const PRIVATE_KEY = process.env.PRIVATE_KEY || '0x';
+// optional
+const MNEMONIC = process.env.MNEMONIC || 'your mnemonic';
+
+// Your API key for Etherscan, obtain one at https://etherscan.io/
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || 'Your etherscan API key';
+const POLYGONSCAN_API_KEY = process.env.POLYGONSCAN_API_KEY || 'Your polygonscan API key';
+const REPORT_GAS = process.env.REPORT_GAS.toLowerCase() === 'true' || false;
+
+module.exports = {
+    solidity: {
+        compilers: [{ version: '0.8.24' }, { version: '0.6.6' }, { version: '0.4.19' }],
+    },
+    defaultNetwork: 'hardhat',
+    networks: {
+        hardhat: {
+            chainId: 31337,
+            forking: {
+              url: MAINNET_RPC_URL
+            },
+        },
+        localhost: {
+            chainId: 31337,
+        },
+        sepolia: {
+            url: SEPOLIA_RPC_URL,
+            accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
+            //   accounts: {
+            //     mnemonic: MNEMONIC,
+            //   },
+            saveDeployments: true,
+            chainId: 11155111,
+            blockConfirmation: 6,
+        },
+        mainnet: {
+            url: MAINNET_RPC_URL,
+            accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
+            //   accounts: {
+            //     mnemonic: MNEMONIC,
+            //   },
+            saveDeployments: true,
+            chainId: 1,
+        },
+        polygon: {
+            url: POLYGON_MAINNET_RPC_URL,
+            accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
+            saveDeployments: true,
+            chainId: 137,
+        },
+    },
+    etherscan: {
+        // yarn hardhat verify --network <NETWORK> <CONTRACT_ADDRESS> <CONSTRUCTOR_PARAMETERS>
+        apiKey: {
+            sepolia: ETHERSCAN_API_KEY,
+            polygon: POLYGONSCAN_API_KEY,
+        },
+        customChains: [
+            {
+                network: 'sepolia',
+                chainId: 11155111,
+                urls: {
+                    apiURL: 'https://api-sepolia.etherscan.io/api',
+                    browserURL: 'https://sepolia.etherscan.io',
+                },
+            },
+        ],
+    },
+    sourcify: {
+        enabled: true,
+    },
+    gasReporter: {
+        enabled: REPORT_GAS,
+        currency: 'USD',
+        outputFile: 'gas-report.txt',
+        noColors: true,
+        // coinmarketcap: process.env.COINMARKETCAP_API_KEY,
+    },
+    contractSizer: {
+        runOnCompile: false,
+        only: ['AToken'],
+    },
+    namedAccounts: {
+        deployer: {
+            default: 0, // here this will by default take the first account as deployer
+            1: 0, // similarly on mainnet it will take the first account as deployer. Note though that depending on how hardhat network are configured, the account 0 on one network can be different than on another
+        },
+        player: {
+            default: 1,
+        },
+    },
+    mocha: {
+        timeout: 600000, // 600 seconds max for running tests
+    },
+};
