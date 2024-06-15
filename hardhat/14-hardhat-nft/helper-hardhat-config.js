4d6db21e8c7b6d4b5675dfd9b971c0d62f23b7be
Rodrigo Sarmiento <info@rodrigosarmiento.com>
2024-06-15 13:34:36 -0600

    refactor(lesson14): update ETH/USD price feed

--- hardhat/14-hardhat-nft/helper-hardhat-config.js
+++ hardhat/14-hardhat-nft/helper-hardhat-config.js
@@ -16,7 +16,7 @@ const networkConfig = {
     11155111: {
         callbackGasLimit: '500000', // 500,000 gas
         ethUsd: '0x694AA1769357215DE4FAC081bf1f309aDC325306',
-        ethUsdPriceFeed: '0x8A753747A1Fa494EC906cE90E9f37563A8AF630e',
+        ethUsdPriceFeed: '0x694AA1769357215DE4FAC081bf1f309aDC325306',
         gasLane: '0x474e34a077df58807dbe9c96d3c009b23b3c6d0cce433e59bbf5b34f823bc56c', // 30 gwei
         keepersUpdateInterval: '30',
         mintFee: '10000000000000000', // 0.01 ETH
