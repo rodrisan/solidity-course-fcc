fb102ed3b48dee206335a60fb2590189c0483f9e
Rodrigo Sarmiento <info@rodrigosarmiento.com>
2024-06-15 11:40:18 -0600

    feat(lesson14): continue Dynamic contract

--- hardhat/14-hardhat-nft/contracts/DynamicSvgNft.sol
+++ hardhat/14-hardhat-nft/contracts/DynamicSvgNft.sol
@@ -1,30 +1,77 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.8;
 
+import '@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol';
 import '@openzeppelin/contracts/token/ERC721/ERC721.sol';
+// The new-version of "@openzeppelin/contracts": "^5.0.1", already has Base64.sol
+// import "@openzeppelin/contracts/utils/Base64.sol";
 import 'base64-sol/base64.sol';
+
+error ERC721Metadata__URI_QueryFor_NonExistentToken();
 
 contract DynamicSvgNft is ERC721 {
     uint256 private s_tokenCounter;
-    string private i_lowImageURI;
-    string private i_highImageURI;
-    string private constant BASE64_ENCODED_SVG_PREFIX = 'data:image/svg+xml;base63,';
+    string private s_lowImageURI;
+    string private s_highImageURI;
 
-    constructor(string memory lowSvg, string memory highSvg) ERC721('Dynamic SVG NFT', 'RDNFT') {
+    mapping(uint256 => int256) public s_tokenIdToHighValues;
+    AggregatorV3Interface internal immutable i_priceFeed;
+    event CreatedNFT(uint256 indexed tokenId, int256 highValue);
+
+    constructor(
+        address priceFeedAddress,
+        string memory lowSvg,
+        string memory highSvg
+    ) ERC721('Dynamic SVG NFT', 'RDNFT') {
         s_tokenCounter = 0;
-        i_lowImageURI = svgToImageURI(lowSvg);
-        i_highImageURI = svgToImageURI(highSvg);
+        s_lowImageURI = svgToImageURI(lowSvg);
+        s_highImageURI = svgToImageURI(highSvg);
+        i_priceFeed = AggregatorV3Interface(priceFeedAddress);
     }
 
     function svgToImageURI(string memory svg) public pure returns (string memory) {
+        string memory baseUrl = 'data:image/svg+xml;base63,';
         string memory svgBase64Encoded = Base64.encode(bytes(string(abi.encodePacked(svg))));
         // ^0.8.12+ : string.concat(stringA, stringB) // Cool!
-        return string(abi.encodePacked(BASE64_ENCODED_SVG_PREFIX, svgBase64Encoded));
+        return string(abi.encodePacked(baseUrl, svgBase64Encoded));
     }
 
-    function mintNft() public {
-        _safeMint(msg.sender, s_tokenCounter);
-        s_tokenCounter += 1;
+    function mintNft(int256 highValue) public {
+        uint256 newTokenId = s_tokenCounter;
+        s_tokenIdToHighValues[newTokenId] = highValue;
+        _safeMint(msg.sender, newTokenId);
+        s_tokenCounter = s_tokenCounter + 1;
+        emit CreatedNFT(newTokenId, highValue);
+    }
+
+    function tokenURI(uint256 tokenId) public view override returns (string memory) {
+        require(_exists(tokenId), 'URI query for nonexistent token');
+        // string memory imageURI = 'test';
+
+        (, int256 price, , , ) = i_priceFeed.latestRoundData();
+        string memory imageURI = s_lowImageURI;
+        if (price >= s_tokenIdToHighValues[tokenId]) {
+            imageURI = s_highImageURI;
+        }
+
+        return
+            string(
+                abi.encodePacked(
+                    _baseURI(),
+                    Base64.encode(
+                        bytes(
+                            abi.encodePacked(
+                                '{"name":"',
+                                name(),
+                                '", "description": "NFT that changes based on the ChainLink feed"',
+                                '"attributes": [{"trait_type": "coolness", "value": 100, "image": "',
+                                imageURI,
+                                '"}]'
+                            )
+                        )
+                    )
+                )
+            );
     }
 
     function _baseURI() internal pure override returns (string memory) {
@@ -33,26 +80,19 @@ contract DynamicSvgNft is ERC721 {
         return 'data:application/json;base64,';
     }
 
-    function tokenURI(uint256 tokenId) public view override returns (string memory) {
-        require(_exists(tokenId), 'URI query for nonexistent token');
-        string memory imageURI = 'test';
+    function getLowSVG() public view returns (string memory) {
+        return s_lowImageURI;
+    }
 
-        return string(
-            abi.encodePacked(
-                _baseURI(),
-                Base64.encode(
-                    bytes(
-                        abi.encodePacked(
-                            '{"name":"',
-                            name(),
-                            '", "description": "NFT that changes based on the ChainLink feed"',
-                            '"attributes": [{"trait_type": "coolness", "value": 100, "image": "',
-                            imageURI,
-                            '"}]'
-                        )
-                    )
-                )
-            )
-        );
+    function getHighSVG() public view returns (string memory) {
+        return s_highImageURI;
+    }
+
+    function getPriceFeed() public view returns (AggregatorV3Interface) {
+        return i_priceFeed;
+    }
+
+    function getTokenCounter() public view returns (uint256) {
+        return s_tokenCounter;
     }
 }
--- hardhat/14-hardhat-nft/helper-hardhat-config.js
+++ hardhat/14-hardhat-nft/helper-hardhat-config.js
@@ -4,23 +4,26 @@ const networkConfig = {
         keepersUpdateInterval: '30',
     },
     31337: {
-        name: 'localhost',
-        subscriptionId: '588',
+        callbackGasLimit: '500000', // 500,000 gas
+        ethUsdPriceFeed: '0x694AA1769357215DE4FAC081bf1f309aDC325306',
         gasLane: '0x474e34a077df58807dbe9c96d3c009b23b3c6d0cce433e59bbf5b34f823bc56c', // 30 gwei
         keepersUpdateInterval: '30',
-        raffleEntranceFee: ethers.utils.parseEther('0.01'), // 0.01 ETH
-        callbackGasLimit: '500000', // 500,000 gas
         mintFee: '10000000000000000', // 0.01 ETH
+        name: 'localhost',
+        raffleEntranceFee: ethers.utils.parseEther('0.01'), // 0.01 ETH
+        subscriptionId: '588',
     },
     11155111: {
-        name: 'sepolia',
-        subscriptionId: '11802', // from VRF Subscription v2.0, see the README (3.2)
+        callbackGasLimit: '500000', // 500,000 gas
+        ethUsd: '0x694AA1769357215DE4FAC081bf1f309aDC325306',
+        ethUsdPriceFeed: '0x8A753747A1Fa494EC906cE90E9f37563A8AF630e',
         gasLane: '0x474e34a077df58807dbe9c96d3c009b23b3c6d0cce433e59bbf5b34f823bc56c', // 30 gwei
         keepersUpdateInterval: '30',
-        raffleEntranceFee: ethers.utils.parseEther('0.01'), // 0.01 ETH
-        callbackGasLimit: '500000', // 500,000 gas
-        vrfCoordinatorV2: '0x8103B0A8A00be2DDC778e6e7eaa21791Cd364625',
         mintFee: '10000000000000000', // 0.01 ETH
+        name: 'sepolia',
+        raffleEntranceFee: ethers.utils.parseEther('0.01'), // 0.01 ETH
+        subscriptionId: '11802', // from VRF Subscription v2.0, see the README (3.2)
+        vrfCoordinatorV2: '0x8103B0A8A00be2DDC778e6e7eaa21791Cd364625',
     },
     1: {
         name: 'mainnet',
