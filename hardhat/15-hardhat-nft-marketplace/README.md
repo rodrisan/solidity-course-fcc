8025596afd48b656d50c341aa608bee673f3a530
Rodrigo Sarmiento <info@rodrigosarmiento.com>
2024-06-25 08:39:53 -0600

    feat(lesson15): update contract references

--- hardhat/15-hardhat-nft-marketplace/README.md
+++ hardhat/15-hardhat-nft-marketplace/README.md
@@ -1,5 +1,12 @@
 # 15 - Hardhat NFT Marketplace
 
-Frontend
-* [Moralis](https://youtu.be/gyMwXuJrbJQ?si=xb3Q4CcfFtBv5Uek&t=89306)
-* [The Graph](https://youtu.be/gyMwXuJrbJQ?si=0xyH2Sv2lNrYcjKb&t=100645)
+## Contracts
+
+-   [NftMarketplace Sepolia](https://sepolia.etherscan.io/address/0x9a55827EB7bEd6db3E7CA42105522F757c30AadD)
+-   [BasicNft Sepolia](https://sepolia.etherscan.io/address/0x348897C4E6c0091AACf9437B204E39005fBCa0ba)
+-   [BasicNftTwo Sepolia](https://sepolia.etherscan.io/address/0x404DD0C626aa08f5aB7faA4FB989d72878728065)
+
+Frontend Videos
+
+-   [Moralis](https://youtu.be/gyMwXuJrbJQ?si=xb3Q4CcfFtBv5Uek&t=89306)
+-   [The Graph](https://youtu.be/gyMwXuJrbJQ?si=0xyH2Sv2lNrYcjKb&t=100645)
--- hardhat/15-hardhat-nft-marketplace/helper-hardhat-config.js
+++ hardhat/15-hardhat-nft-marketplace/helper-hardhat-config.js
@@ -30,11 +30,11 @@ const networkConfig = {
 
 const developmentChains = ['hardhat', 'localhost'];
 const VERIFICATION_BLOCK_CONFIRMATIONS = 6;
-const frontEndContractsFile = '../nextjs-nft-marketplace-moralis-fcc/constants/networkMapping.json';
+const frontEndContractsFile = '../15-nextjs-nft-marketplace-moralis/constants/networkMapping.json';
 const frontEndContractsFile2 =
-    '../nextjs-nft-marketplace-thegraph-fcc/constants/networkMapping.json';
-const frontEndAbiLocation = '../nextjs-nft-marketplace-moralis-fcc/constants/';
-const frontEndAbiLocation2 = '../nextjs-nft-marketplace-thegraph-fcc/constants/';
+    '../15-nextjs-nft-marketplace-the-graph/constants/networkMapping.json';
+const frontEndAbiLocation = '../15-nextjs-nft-marketplace-moralis/constants/';
+const frontEndAbiLocation2 = '../15-nextjs-nft-marketplace-the-graph/constants/';
 
 module.exports = {
     networkConfig,
